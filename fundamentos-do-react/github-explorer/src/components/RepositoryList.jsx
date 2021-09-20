1385f08d1c670156a5cd346ffd0c95ffc8fa86d3
Banzak-Uninter <leosantanabr@gmail.com>
2021-09-20 15:11:59 -0300

    estilização da lista de repositórios

--- fundamentos-do-react/github-explorer/src/components/RepositoryList.jsx
+++ fundamentos-do-react/github-explorer/src/components/RepositoryList.jsx
@@ -1,8 +1,9 @@
 import { RepositoryItem } from "./RepositoryItem";
+import '../styles/repositories.scss';
 
 export function RepositoryList() {
     return (
-        <section className="repositori-list">
+        <section className="repositories-list">
             <h1>Lista de Repositório</h1>
             <ul>
                 <RepositoryItem repository="unform2"/>

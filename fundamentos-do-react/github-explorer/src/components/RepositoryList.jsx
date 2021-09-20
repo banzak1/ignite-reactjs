2f619a72daaab69e628b09b3727ff215d15d4b28
Banzak-Uninter <leosantanabr@gmail.com>
2021-09-20 15:29:59 -0300

    requisitando as informações com a api do github

--- fundamentos-do-react/github-explorer/src/components/RepositoryList.jsx
+++ fundamentos-do-react/github-explorer/src/components/RepositoryList.jsx
@@ -1,7 +1,20 @@
 import { RepositoryItem } from "./RepositoryItem";
 import '../styles/repositories.scss';
+import { useEffect, useState } from "react";
+
+// https://api.github.com/users/banzak1/repos
+
 
 export function RepositoryList() {
+
+    const [repositories, setRepositories] = useState([]);
+
+    useEffect(() => {
+        fetch('https://api.github.com/users/banzak1/repos')
+        .then(response => response.json())
+        .then(data => setRepositories(data))
+    }, []);
+
     return (
         <section className="repositories-list">
             <h1>Lista de Repositório</h1>
